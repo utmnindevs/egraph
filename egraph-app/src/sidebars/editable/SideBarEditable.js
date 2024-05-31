364f4b2676ea76b238ae49951c46523f46dabf55
waomi69 <ikuldarev@mail.ru>
2024-05-31 23:53:43 +0500

    fix editablesidebar info

--- egraph-app/src/sidebars/editable/SideBarEditable.js
+++ egraph-app/src/sidebars/editable/SideBarEditable.js
@@ -4,6 +4,7 @@ import { useForm, Controller } from "react-hook-form";
 import "./style/SideBarEditable.css"
 
 import { EditableFormInputs } from '../../handlers/CompartmentForms';
+
 /**
  * Создает правую панель редактирования, в соответствии с выбранным узлом.
  * Учитывает тип узла и генерирует для него определенные параметры для редактирования.
@@ -12,54 +13,58 @@ import { EditableFormInputs } from '../../handlers/CompartmentForms';
  * @returns 
  */
 function SideBarEditable({ node, setStateMenu, e_graph, updateGraphNodes, setGraphNodes, ...editable_props }) {
-    const { register, handleSubmit, setError, formState: { errors } } = useForm({mode: 'onSubmit', defaultValues: {
+    const { register, handleSubmit, setError, formState: { errors }, reset } = useForm({mode: 'onSubmit', defaultValues: {
         name: node.data.name,
         population: node.data.population,
         ins: node.data.ins,
         outs: node.data.outs
     }});
 
+    useEffect(() => {
+        reset({
+            name: node.data.name,
+            population: node.data.population,
+            ins: node.data.ins,
+            outs: node.data.outs
+        });
+    }, [node, reset]);
+
     /**
-     * Метод вызывается во время нажатия на кнопку "Принять". Обновляет текущий выбранный узел согласно обновленным данных узлов
+     * Метод вызывается во время нажатия на кнопку "Принять". Обновляет текущий выбранный узел согласно обновленным данным узлов
      * через стейты
      */
     const onCompleteSubmit = useCallback((data) => {
-
         setGraphNodes((nds) => {
             return nds.map((nd) => {
                 if (nd.id === node.id) {
                     e_graph.getCompartmentByName(node.data.name).UpdateCompartment(data.name, parseFloat(data.population))
                     nd.data = { ...node.data, ins: parseInt(data.ins), outs: parseInt(data.outs) }
-                    // TODO: проверка на существование подключенных узлов, если есть, то не менять и крикнуть пользователя
-                    // иначе менять, т.к. появляется баг, что при удалении ребро остается, и после очередного добавления в него можно снова присоеденить
                 }
                 return nd;
             })
         })
         updateGraphNodes(e_graph.GetComps())
-
-
         setStateMenu(null);
-    }, [updateGraphNodes])
+    }, [updateGraphNodes, node, setGraphNodes, e_graph, setStateMenu]);
 
     const onSubmit = useCallback((form_data) => {
         onCompleteSubmit(form_data);
-    }, [updateGraphNodes]);
+    }, [onCompleteSubmit]);
 
     return (
         <div class="side-bar-editable-body">
             <p class="header"> Редактирование {node.type === 'compartmentNode' ? "компартмента" : "потока"} </p>
             <form className='form-content'>
-            <div class="content">
-                <EditableFormInputs errors={errors} register={register}/>
-                <div class="form-check"> {/* где проверка то на булево? */}
-                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
-                    <label class="form-check-label" for="flexCheckDefault">
-                        Стартовый компартмент
-                    </label>
+                <div class="content">
+                    <EditableFormInputs errors={errors} register={register}/>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                        <label class="form-check-label" for="flexCheckDefault">
+                            Стартовый компартмент
+                        </label>
+                    </div>
                 </div>
-            </div>
-            <button class='button-39 footer foot-button' onClick={handleSubmit(onSubmit)}>Принять</button> {/* Добавить кейкап на enter */}
+                <button class='button-39 footer foot-button' onClick={handleSubmit(onSubmit)}>Принять</button>
             </form>
         </div>
     )
